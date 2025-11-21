193316175f34a50a49c4143f163600b29e56038e
mayuri shirsat <mayurisantosh412@gmail.com>
2025-11-21 15:56:21 +0530

    que 3 to 6

--- ch2/practice2.py
+++ ch2/practice2.py
@@ -1,5 +1,5 @@
 # PRACTICE SET2
-# Que1->write porgram to add two no.
+Que1->write porgram to add two no.
 a=float(input("Enter 1st no:"))
 b=float(input("Enter 2nd no:"))
 print("Addition is:",a+b)
@@ -9,3 +9,20 @@ a=12
 b=5
 print("reminder is",a%b)
 
+# Que3->check the type of veriable assigned using input() function
+a=input("Enter nay no:")
+print(type(a))
+
+# Que4->use comparision operater to find out weaather the given variable a is greater than b or not take a=34 and b=80
+a=int(input("Enter any no:"))
+b=int(input("Enter any no:"))
+print("a is greater than b is",a>b)
+
+# Que5->program to find an average of two no entered by the user
+a=int(input("Enter any no:"))
+b=int(input("Enter any no:"))
+print("Average of entered no is",(a+b)/2)
+
+# Que6->porgram to calculate square of no entered by user
+a=int(input("Enter any no:"))
+print("square of entered no is",a**2)#or (a*a)
