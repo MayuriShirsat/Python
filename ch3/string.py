557d06d19af461646a885c6646702f34fbd2e54f
mayuri shirsat <mayurisantosh412@gmail.com>
2025-11-21 17:30:09 +0530

    steing functions

--- ch3/string.py
+++ ch3/string.py
@@ -8,6 +8,7 @@ way to write string->
 a="mayuri"
 b='santosh'
 c='''shirsat'''
+
 # string slicing
 # index start from "0" if form left and from "-1" if from right(negative slicing)
 d=a[0:3]#from 0 to 1 less than 3(excluding 3)
@@ -18,3 +19,23 @@ print(a[-4:-1])#simple way convert -ve indexing in +ve ex., -4:-1 become 2:5
 print(a[:4])
 print(a[2:])
 print(a[0:6:2])#print from 'm' to 'r' but at a gap of 2
+
+# string function
+print(len(b))
+print(b.endswith("sh"))#is string ends with "sh"
+print(b.startswith("san"))
+print(b.upper())
+print(b.capitalize())
+print(b.find("an"))
+print(b.count("s"))
+s="hello world"
+print(s.replace("world","python"))
+
+# Escape sequence characters
+'''
+\n->for enw line
+\t->for tab
+\"->for "" inside ""
+\\->for actual backslash symbol
+'''
+print("hello\nI Am Mayuri\nI am learning python\nI like to learn new things")
