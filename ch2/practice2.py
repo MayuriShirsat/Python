29ca93a158514d46cd52bfb2c6ec9878f1426c6f
mayuri shirsat <mayurisantosh412@gmail.com>
2025-11-21 15:59:00 +0530

    adding a comment

--- ch2/practice2.py
+++ ch2/practice2.py
@@ -1,5 +1,5 @@
 # PRACTICE SET2
-Que1->write porgram to add two no.
+# Que1->write porgram to add two no.
 a=float(input("Enter 1st no:"))
 b=float(input("Enter 2nd no:"))
 print("Addition is:",a+b)
