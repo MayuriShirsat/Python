7d5f7ef77932b655953e5c2f7eb15b0a98146149
mayuri shirsat <mayurisantosh412@gmail.com>
2025-11-22 13:21:16 +0530

    dictionary methodes

--- ch5/dictionary & sets.py
+++ ch5/dictionary & sets.py
@@ -6,6 +6,7 @@ a={
    "list":[1,4,6],
    0:"sun"
    }
+
 print(a,type(a))
 print(a["key"]) #prints curresponding value
 print(a["list"]) #print [1,4,6]
@@ -15,3 +16,12 @@ print(a["list"]) #print [1,4,6]
 # it is indexed
 # it can not contain duplicate keys
 
+# dictionary methodes
+print(a.items())
+print(a.keys())
+print(a.values())
+a.update({"sandhya":99,"marks":100})
+print(a)
+print(a.get("mayuri")) # this do same wark as "a["mayuri"]" as above but note following
+print(a.get("shiv")) # it give output as NONE as shiv is not in a
+print(a["shiv"]) # but this give ERROR as shiv is not in a
