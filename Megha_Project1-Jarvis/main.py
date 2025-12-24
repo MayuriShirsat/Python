8b7f76ee732906616fbafae65bdba8ea9564bf4d
mayuri shirsat <mayurisantosh412@gmail.com>
2025-12-24 19:08:17 +0530

    seting the environment for project

---
+++ Megha_Project1-Jarvis/main.py
@@ -0,0 +1,27 @@
+# PyAudio ❌ does NOT support Python 3.14
+# PyAudio ✅ works with Python 3.11
+
+# first need to install python version 3.11
+''' 
+1️⃣ Installing new Python version does NOT delete old versions
+On Windows:
+Each Python version installs into its own folder
+They coexist safely
+2️⃣ A virtual environment does NOT install Python.
+It only copies/links an existing Python version
+3️⃣ Which version runs when you type python
+This is controlled by:Python Launcher (py)
+ex.,
+py -3.11 -m venv audioenv
+py -3.14 -m venv venv
+
+audioenv → Python 3.11
+venv → Python 3.14
+'''
+# py -3.11 -m venv venv
+# py -3.11 -m venv --> “Use Python 3.11 to create a virtual environment”
+# venv(name of virtual environment)
+
+# .\venv\Scripts\activate.ps1
+
+# pip install speechrecognition pyaudio
