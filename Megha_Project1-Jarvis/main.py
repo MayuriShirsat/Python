ed3b0da753063681dd62eb5d5a207b29600d80f5
mayuri shirsat <mayurisantosh412@gmail.com>
2025-12-25 08:48:23 +0530

    finish with starting conversation and responce

--- Megha_Project1-Jarvis/main.py
+++ Megha_Project1-Jarvis/main.py
@@ -22,12 +22,49 @@ venv → Python 3.14
 # py -3.11 -m venv --> “Use Python 3.11 to create a virtual environment”
 # venv(name of virtual environment)
 
+# cd c:\Python\Megha_Project1-Jarvis(to make the current directry in the forlder we are working on)
 # .\venv\Scripts\activate.ps1
+#                                       OR
+# go to interpreter and select venv-->python 3.11(this automatically created venv)
 
 # pip install speechrecognition pyaudio
 # pip install setuptools
+# pip install pyttsx3
 '''
 PyAudio:A Python library used to capture audio from the microphone.
 SpeechRecognition:A Python library used to convert spoken language into text.
 Setuptools:A utility library used for building and installing Python packages.
+pyttsx3:Convert text to speech
 '''
+
+import speech_recognition as sr
+import webbrowser # builtin module-->It allows Python to open URLs in a web browser automatically.
+import pyttsx3
+
+r= sr.Recognizer() # for simplicity (audio to text)
+engine= pyttsx3.init() # for initilisation (text to speach)
+
+# jarvis response
+def speak(text):
+    engine.say(text) # (text to speach)
+    engine.runAndWait() # otherwise program will exit before reading the text
+
+if __name__=="__main__":
+    speak("Hey Mayuri!! how may I help you.")
+    while True:
+        # lisition for the word "Jarvis" and then only respond
+        try:
+            # obtain audio from the microphone 
+            with sr.Microphone() as source: # take audio from microphone
+                print("listening....")
+                audio= r.listen(source,timeout=3,phrase_time_limit=2)
+                # timeout-->starting timeout(before speaking)
+                # phrase_time_limit-->time space between words
+
+            # recognize speech using google recognizer    
+            command=r.recognize_google(audio) # speech_recognition-->Recognizer() class -->recognize_google() subclass
+            print(command)
+            
+        except Exception as e:
+            print("Error; {0}".format(e))
+
